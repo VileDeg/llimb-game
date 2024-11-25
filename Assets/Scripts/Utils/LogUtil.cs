fc942163745b4e07f2bd29a2427e1e958a6cecbe
VileDeg <gvadim134@gmail.com>
2024-11-25 03:16:44 +0100

    Integrate new Input System, add test scene

---
+++ Assets/Scripts/Utils/LogUtil.cs
@@ -0,0 +1,28 @@
+public static class LogUtil
+{
+    public static void Info(string message)
+    {
+        string callerInfo = getCallerInfo();
+        UnityEngine.Debug.Log($"[{callerInfo}] {message}");
+    }
+
+    public static void Warning(string message)
+    {
+        string callerInfo = getCallerInfo();
+        UnityEngine.Debug.LogWarning($"[{callerInfo}] {message}");
+    }
+
+    public static void Error(string message)
+    {
+        string callerInfo = getCallerInfo();
+        UnityEngine.Debug.LogError($"[{callerInfo}] {message}");
+    }
+
+    private static string getCallerInfo()
+    {
+        System.Diagnostics.StackTrace stackTrace = new System.Diagnostics.StackTrace();
+        System.Diagnostics.StackFrame stackFrame = stackTrace.GetFrame(1);
+        string callerInfo = $"{stackFrame.GetMethod().DeclaringType}.{stackFrame.GetMethod().Name}";
+        return callerInfo;
+    }
+}
