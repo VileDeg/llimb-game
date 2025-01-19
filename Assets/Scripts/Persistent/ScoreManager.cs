d4862dcf0fd06d44c7986538215e3cd54181680c
VileDeg <gvadim134@gmail.com>
2025-01-19 21:17:57 +0100

    fix: Score manager missing text

--- Assets/Scripts/Persistent/ScoreManager.cs
+++ Assets/Scripts/Persistent/ScoreManager.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScoreManager : SingletonBase<ScoreManager>
 {
@@ -7,8 +8,27 @@ public class ScoreManager : SingletonBase<ScoreManager>
     private TextMeshProUGUI scoreText;
 
     private int _score;
-    
+
+    protected override void Awake()
+    {
+        base.Awake();
+        // Subscribe to the sceneLoaded event
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDestroy()
+    {
+        // Unsubscribe from the event to prevent memory leaks
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
     private void Start()
+    {
+        // Do not init in Start, it gets called OnSceneLoaded, otherwise would be called twice!
+        // InitScore();
+    }
+
+    private void InitScore()
     {
         _score = PlayerPrefs.GetInt("Score", 0);
         UpdateScoreText();
@@ -22,8 +42,34 @@ public class ScoreManager : SingletonBase<ScoreManager>
 
     private void UpdateScoreText()
     {
-        scoreText.text = "Score: " + _score;
-        Debug.Log($"Score updated: {_score}");
+        if (!TryFindScoreTextIfNull()) {
+            return;
+        }
+        if (scoreText != null) {
+            scoreText.text = "Score: " + _score;
+            Debug.Log($"Score updated: {_score}");
+        } else {
+            Debug.LogError("ScoreText object not found in the scene.");
+        }
+    }
+
+    // TODO: Find better approach to find Score text in scene.
+    private bool TryFindScoreTextIfNull()
+    {
+        if (scoreText != null) {
+            return true;
+        }
+
+        // Attempt to find the ScoreText object by name
+        GameObject scoreObject = GameObject.Find("Score");
+        if (scoreObject != null) {
+            scoreText = scoreObject.GetComponent<TextMeshProUGUI>();
+            return true;
+        } else {
+            Debug.LogWarning("ScoreText object not found. Ensure there is a UI element named 'Score' in the scene.");
+        }
+
+        return false;
     }
 
     public int GetScore()
@@ -44,4 +90,19 @@ public class ScoreManager : SingletonBase<ScoreManager>
         PlayerPrefs.SetInt("Score", _score);
         PlayerPrefs.Save();
     }
+
+
+    /* *******************************
+     * EVENT CALLBACKS
+     * *******************************/
+
+
+    // Callback function called when a new scene is loaded
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log($"Scene loaded: {scene.name}, Mode: {mode}");
+
+        InitScore();
+    }
+
 }
